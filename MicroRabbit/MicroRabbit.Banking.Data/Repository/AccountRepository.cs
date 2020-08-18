fb06ca29cfd77cd2ec751db337e31b286d28c29e
stefan.sadovek <stefan.sadovek@fincore.com>
2020-08-18 13:34:00 +0200

    Partially completed Banking Microservice

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+	public class AccountRepository : IAccountRepository
+	{
+		private readonly BankingDbContext context;
+
+		public AccountRepository(BankingDbContext context)
+		{
+			this.context = context;
+		}
+
+		public IEnumerable<Account> GetAccounts()
+		{
+			return context.Accounts;
+		}
+	}
+}
